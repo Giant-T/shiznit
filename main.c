cddd0da59bfbe4318313ec687f08d6ac52a2a733
William Boudreault <boudreaultwilliam@gmail.com>
2024-02-29 20:41:18 -0500

    ray casting

--- main.c
+++ main.c
@@ -3,9 +3,26 @@
 #include <signal.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-#define CIRCLE_RADIUS 20
 #define PI 3.14159265
+
+typedef struct {
+    double x;
+    double y;
+    double z;
+} vector_t;
+
+typedef struct {
+    vector_t pos;
+    double radius;
+} sphere_t;
+
+typedef struct {
+    vector_t pos;
+    vector_t direction;
+    double distance;
+} camera_t;
 
 unsigned long out_mode = 0;
 HANDLE out_handle;
@@ -60,17 +77,41 @@ boolean initialise_vt(void) {
     return true;
 }
 
-void display(COORD screen_size) {
-    short middle_x = screen_size.X / 2 - 1;
-    short middle_y = screen_size.Y / 2 - 1;
+double vector_len(const vector_t *vec) {
+    return sqrt(vec->x * vec->x + vec->y * vec->y + vec->z * vec->z);
+}
 
-    for (short y = middle_y - CIRCLE_RADIUS; y < middle_y + CIRCLE_RADIUS; y++) {
-        for (short x = middle_x - CIRCLE_RADIUS; x < middle_x + CIRCLE_RADIUS; x++) {
-            short x_diff = abs(x - middle_x);
-            short y_diff = abs(y - middle_y);
-            short distance = sqrt(x_diff * x_diff + y_diff * y_diff);
-            if (distance < CIRCLE_RADIUS) {
-                printf("\x1b[%d;%dH#", y, x);
+vector_t cross_product(const vector_t *vec1, const vector_t *vec2) {
+    vector_t product = {
+        .x = (vec1->y * vec2->z) - (vec2->y * vec1->z),
+        .y = (vec1->x * vec2->z) - (vec2->x * vec1->z),
+        .z = (vec1->x * vec2->y) - (vec2->x * vec1->y),
+    };
+
+    return product;
+}
+
+double ray_distance(const vector_t *ray, const vector_t *vec) {
+    vector_t product = cross_product(ray, vec);
+    double numerator = vector_len(&product);
+    double denumerator = vector_len(vec);
+
+    return numerator / denumerator;
+}
+
+void display(COORD *screen_size, const camera_t *camera, const sphere_t *sphere) {
+    for (int screen_y = 1; screen_y < screen_size->Y; screen_y++) {
+        for (int screen_x = 1; screen_x < screen_size->X; screen_x++) {
+            vector_t ray = {
+                .x = screen_x - (int)(screen_size->X / 2),
+                .y = camera->distance,
+                .z = screen_y - (int)(screen_size->Y / 2),
+            };
+
+            double distance = ray_distance(&ray, &sphere->pos);
+
+            if (distance < sphere->radius) {
+                printf("\x1b[%d;%dH#", screen_y, screen_x);
             }
         }
     }
@@ -89,8 +130,26 @@ int main(void) {
     COORD size = screen_buffer_info.dwSize;
     int counter = 0;
 
+    const sphere_t sphere = {
+        .pos = {
+            .x = 0,
+            .z = 0,
+            .y = 10,
+        },
+        .radius = 20,
+    };
+
+    const camera_t camera = {
+        .pos = {
+            .x = 0,
+            .y = 0,
+            .z = 0,
+        },
+        .distance = 1,
+    };
+
     while (1) {
-        display(size);
+        display(&size, &camera, &sphere);
         printf("\x1b[1;1H%d, size: %d, %d", counter++, size.X, size.Y);
         Sleep(500);
     }
